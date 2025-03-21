acc3f8c7903392561fb27c4cd4c6630e0e54b9c9
Ivanaricci <ivana.ricci26@gmail.com>
2025-03-21 17:41:55 +0100

    [UPD] Es2 da completare

--- js/script.js
+++ js/script.js
@@ -38,3 +38,43 @@ else{
 // Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 
 
+// Utente inserisce pari o dispari (prompt) [evenodd]
+// utente unserisce un numero da 1 a 5prompt [numUser]
+// dichiariamo una funzione che mi deve restituire un risultato (se la parola che ottengo è uguale a ciò che ha inserito l'utente)
+// nella funzione generiamo un numero random per il pc
+// sommiamo il numero del pc con ul numero dell'utente
+// se il numero è divisibile per 2 allora è pari
+// se il numero non è divisibile per 2 allora è dispari
+// fuori dalla funzione -> se l'output della funzione è uguale a evenodd stampiamo hai vinto
+// altrimenti stampiamo hai perso
+
+
+let playUser = prompt('Scegli pari o dispari')
+console.log(playUser)
+
+let numUser = parseInt(prompt('inserisci un numero da 1 a 5'))
+console.log(numUser)
+
+
+function evenOdd (x,y){
+    let result= ''
+    // creo un numero random del pc
+    let numPc = Math.floor(Math.random() * 5) + 1
+    console.log(numPc)
+
+    // sommo il numero generato con quello dell'utente
+    let sum = numPc+numUser
+    console.log(sum)
+
+    if(sum % 2 === 0){
+        result = 'pari'
+    }
+
+    else{
+        result = 'dispari'
+    }
+
+    return result
+
+}
+
